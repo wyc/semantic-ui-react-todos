241ade3796aa81d3deb78970fe5c837e87d2fa9e
Roberto Lezama Gutierrez <rlezama@RLGMBP-08.local>
2018-02-28 23:27:43 -0600

    Upgrading React, PropTypes, Redux, Scripts and Semantic-UI (including React-Semantic-UI) packages. Plus small changes in files where old React.PropTypes was used, now they are using prop-types.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { Button } from 'semantic-ui-react'
 
 const Link = ({ active, children, onClick }) => {
